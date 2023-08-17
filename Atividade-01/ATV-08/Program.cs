555205d5816f5def006c873cb9590f8a49c69694
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2023-08-17 15:48:49 -0400

    ATV 08

---
+++ Atividade-01/ATV-08/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] A = new int[10]; int[] B = new int[10]; int[] C = new int[10];
+int count = 1;
+
+for (int i = 0; i < A.Length; i++)
+{
+    Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+    Console.Write($"Digite o {count}° numero para A:\n>> ");
+    A[i] = Convert.ToInt32(Console.ReadLine());
+    Console.Write($"Digite o {count}° numero para B:\n>> ");
+    B[i] = Convert.ToInt32(Console.ReadLine());
+    count++;
+    Console.Clear();
+}
+Console.WriteLine("-=-=-=-=-=-=-=-=- A soma de A e B -=-=-=-=-=-=-=-=-\n");
+for (int i = 0; i < A.Length; i++)
+{
+    C[i] = A[i] + B[i];
+    Console.WriteLine($"> Soma de A[{A[i]}] + B[{B[i]}] = C[{C[i]}]");
+}
