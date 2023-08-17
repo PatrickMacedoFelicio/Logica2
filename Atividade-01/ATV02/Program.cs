f701d4ec5a941d9f0c03fa118c3d813e9330136f
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2023-08-16 20:40:19 -0400

    update

--- Atividade-01/ATV02/Program.cs
+++ Atividade-01/ATV02/Program.cs
@@ -1,4 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-int semana[] = new int[7];
+int[] semana = new int[7];
+
+for (int i = 0; i < semana.Length; i++)
+{
+
+}
