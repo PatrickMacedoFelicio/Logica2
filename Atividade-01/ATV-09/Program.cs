0f2f106819637546d75d342384eef5950ad858d9
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2023-08-17 16:17:50 -0400

    Atividade 9

---
+++ Atividade-01/ATV-09/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] A = new int[10]; int[] B = new int[10]; int [] C = new int[10];
+int count = 1;
+
+for (int i = 0; i < A.Length; i++)
+{
+    Console.Write($"-- DIgite o {count}° número A:\n>> ");
+    A[i] = Convert.ToInt32(Console.ReadLine());
+    Console.Write($"-- DIgite o {count}° número B:\n>> ");  
+    B[i] = Convert.ToInt32(Console.ReadLine());
+    count++;
+}
