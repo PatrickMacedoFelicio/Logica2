1e3549f4af387aec231d9e73a7e7d1a83d3185c2
Patrick Macêdo Felicio <patrickmacedofeliceo@gmail.com>
2023-08-21 20:49:20 -0400

    update

--- Atividade-01/ATV-09/Program.cs
+++ Atividade-01/ATV-09/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int[] A = new int[10]; int[] B = new int[10]; int [] C = new int[10];
-int count = 1;
+int count = 1, tam = 5;
+int[] A = new int[tam]; int[] B = new int[tam]; int [] C = new int[A.Length + B.Length];
 
 for (int i = 0; i < A.Length; i++)
 {
@@ -10,4 +10,18 @@ for (int i = 0; i < A.Length; i++)
     Console.Write($"-- DIgite o {count}° número B:\n>> ");  
     B[i] = Convert.ToInt32(Console.ReadLine());
     count++;
+    C[i] = A[i];
+}
+for (int i = 0; i < A.Length; i++)
+{
+    for (int j = 0; j < B.Length; j++)
+    {
+        if (C[i] == B[j])
+            C[(A.Length + B.Length)+i] = B[j];
+    }
+}
+for (int i = 0; i < C.Length; i++)
+{
+    Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+    Console.Write(C[i]+", ");
 }
