39dfffa5d77a2cec71a16769d80bee189ee86720
Patrick Macêdo Felicio <patrickmacedofeliceo@gmail.com>
2023-08-22 19:52:02 -0400

    update

--- Atividade-01/ATV-09/Program.cs
+++ Atividade-01/ATV-09/Program.cs
@@ -1,27 +1,100 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int count = 1, tam = 5;
-int[] A = new int[tam]; int[] B = new int[tam]; int [] C = new int[A.Length + B.Length];
+/*int tamanho = 10;
 
-for (int i = 0; i < A.Length; i++)
+double[] A = new double[tamanho];
+double[] B = new double[tamanho];
+double[] C = new double[tamanho * 2];
+
+// I) Preenchendo os vetores/arrays A e B
+Console.Write("\r\n ┌────────────────────────────────────────┐");
+Console.Write("\r\n │ União de 10 valores em ordem crescente │");
+Console.Write("\r\n └────────────────────────────────────────┘\r\n");
+Console.WriteLine("");
+
+for (int i = 0; i < tamanho; i++)
 {
-    Console.Write($"-- DIgite o {count}° número A:\n>> ");
-    A[i] = Convert.ToInt32(Console.ReadLine());
-    Console.Write($"-- DIgite o {count}° número B:\n>> ");  
-    B[i] = Convert.ToInt32(Console.ReadLine());
-    count++;
-    C[i] = A[i];
+    Console.Write(" Digite o valor para A" + (i + 1) + ": ");
+    A[i] = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write(" Digite o valor para B" + (i + 1) + ": ");
+    B[i] = Convert.ToDouble(Console.ReadLine());
 }
-for (int i = 0; i < A.Length; i++)
+
+// II) Criando o vetor/array C com as somas em ordem crescente
+int numA = 0;
+int numB = tamanho;
+
+for (int i = 0; i < tamanho * 2; i++)
 {
-    for (int j = 0; j < B.Length; j++)
+    if (numA < tamanho && (numB >= tamanho * 2 || A[numA] <= B[numB - tamanho]))
     {
-        if (C[i] == B[j])
-            C[(A.Length + B.Length)+i] = B[j];
+        C[i] = A[numA];
+        numA++;
+    }
+    else
+    {
+        C[i] = B[numB - tamanho];
+        numB++;
     }
 }
+
+// III) Imprimindo os vetores/arrays A, B e C
+Console.WriteLine("\r\n -----------------------------------------");
+
+Console.Write("\r\n ┌────────────┐");
+Console.Write("\r\n │ Resultados │");
+Console.Write("\r\n └────────────┘\r\n");
+
+Console.WriteLine(" \r\n Valores A (inseridos): " + string.Join(", ", A));
+Console.WriteLine(" Valor B (inseridos): " + string.Join(", ", B));
+Console.WriteLine(" Valor C (união em ordem crescente): " + string.Join(", ", C));
+
+Console.WriteLine("");
+Console.WriteLine("\r\n Pressione ENTER para finalizar.");
+Console.WriteLine("\r\n -----------------------------------------");
+
+Console.ReadKey();*/
+
+int tam = 10, numA = 0, numB = tam, count = 1;
+int[] A = new int[tam], B = new int[tam], C = new int[tam*2];
+
+Console.WriteLine("-=-=-=-=-=-=-=-=-=-=- União de 10 Números em Ordem Crescente -=-=-=-=-=-=-=-=-=-=-=");
+
+for (int i = 0; i < tam; i++)
+{
+    Console.Write($"\n- Digite o valor do {count}º número de A: ");
+    A[i] = Convert.ToInt32(Console.ReadLine());
+    Console.Write($"- Digite o valor do {count}º número de A: ");
+    B[i] = Convert.ToInt32(Console.ReadLine());
+    count++;
+}
+
+for (int i = 0; i < tam*2; i++)
+{
+    if (numA < tam && (numB >= tam * 2 || A[numA] <= B[numB-tam])){
+        C[i] = A[numA];
+        numA++;
+    }else{
+        C[i] = B[numB - tam];
+        numB++;
+    }
+}
+
+Console.WriteLine("\n-=-=-=-=-=-=-=-=-=-=- União de 10 Números em Ordem Crescente -=-=-=-=-=-=-=-=-=-=-=");
+
+Console.Write("- Os valores inseridos em A foram: ");
+for (int i = 0; i < A.Length; i++)
+{
+    Console.Write($"{A[i]}, ");
+}
+Console.Write("\n- Os valores inseridos em B foram: ");
+for (int i = 0; i < B.Length; i++)
+{
+    Console.Write($"{B[i]}, ");
+}
+Console.Write("\n- A união dos Valores colocado em C na ordem crescete é: ");
 for (int i = 0; i < C.Length; i++)
 {
-    Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-    Console.Write(C[i]+", ");
+    Console.Write($"{C[i]}, ");
 }
