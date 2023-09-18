e2139cb7d14fd7275ad5ee5d74d12d0c6f4cde4e
Patrick Macêdo Felicio <patrickmacedofeliceo@gmail.com>
2023-09-18 19:13:59 -0400

    update

--- Atividade-03/ATV09/Program.cs
+++ Atividade-03/ATV09/Program.cs
@@ -11,7 +11,7 @@ namespace ATV09
         static double[] teste;
         static void Main(string[] args)
         {
-            teste  = new double[5];
+            teste  = new double[30];
 
             for (int i = 0; i < teste.Length; i++)
             {
