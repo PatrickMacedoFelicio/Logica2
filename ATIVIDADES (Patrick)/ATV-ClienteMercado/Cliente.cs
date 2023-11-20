2cc2bf7ccf3fb948bc1f8c7534ca35332dc12d15
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2023-11-20 00:04:00 -0400

    update

---
+++ ATIVIDADES (Patrick)/ATV-ClienteMercado/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+// 1 passo : criar a lista com os atributos
+namespace ListaCliente
+{
+    public class Cliente
+    {
+        public string nome;
+        public int id;
+        public double[]? valorproduto = new double[0];
+        public string[]? nomeproduto = new string[0];
+        public Cliente? prox;
+        public Endereco? endereco;
+
+
+    }
+}
